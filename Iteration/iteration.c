8bb94053d129459e8e56e54e20a78bef6fe284c3
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2023-09-20 09:19:59 +0530

    upload iteration.c

--- Iteration/iteration.c
+++ Iteration/iteration.c
@@ -2,15 +2,18 @@
 #include<math.h>
 
 double f(double x){
-    return (x*x*x - 2*x - 4);
+    return (2*x - log10(x) - 7);
+	// return (x*x*x - 2*x*x - 4);
 }
 
 double g(double x){
-	return pow((2*x*x+4), (1/3));
+	return (7+log10(x))/2;
+	// return cbrt(2*x*x+4);
 }
 
 double h(double x){
-	return (4*x*pow((2*x*x+4), (-2/3)))/3;
+	return 1/(2*x);
+	// return 4*x/(3*cbrt(pow(2*x*x+4,2)));
 }
 
 int i=0;
@@ -26,7 +29,6 @@ void iteration(double a){
         printf("Steps are - %d \n", i);
         return;
     }
-    
     iteration(x);
 }
 
@@ -34,7 +36,7 @@ int main()
 {
     double a, b, m;
     int z=1;
-    printf("The equation is =  x^3 - 4*x - 9. \n");
+    printf("The equation is =  2*x - log10(x) - 7. \n");
 
     do{
         printf("Enter the first root = ");
@@ -43,7 +45,7 @@ int main()
         printf("Enter the second root = ");
         scanf("%lf", &b);
         m=(a+b)/2;
-        if(fabs(h(a))<1 && fabs(h(b))<1 && fabs(h(m))<1){
+        if(fabs(h(a))<1 && fabs(h(b))<1 && fabs(h(m))){
             printf("Roots are accepted.\n");
             z=0;
         }
