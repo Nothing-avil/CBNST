a8059792e253931213d38e946ba278b4f502046b
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2023-11-08 09:51:56 +0530

    Create trapezoidal.c

---
+++ Trapizodial/trapezoidal.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+#define f(x) (1/(1+x*x))
+int main(){
+  int n;
+  float a, b, h;
+  printf("Enter the number of Intervals = ");
+  scanf("%d",&n);
+  
+  printf("Enter the value of a and b = ");
+  scanf("%f%f",&a,&b);
+  
