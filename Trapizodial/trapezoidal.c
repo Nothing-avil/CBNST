a945fec2788f7d8b07e218550f20eb67e3b7ab7b
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2023-12-19 16:44:14 +0530

    Update trapezoidal.c

--- Trapizodial/trapezoidal.c
+++ Trapizodial/trapezoidal.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<math.h>
-#define f(x) (1/(1+x*x))
+#define f(x) (1/(1+(x*x)))
 int main(){
   int n;
   float a, b, h;
@@ -9,4 +9,13 @@ int main(){
   
   printf("Enter the value of a and b = ");
   scanf("%f%f",&a,&b);
-  
+  h = (b-a)/n;
+  printf("%f %f %f\n",a, b, h);
+  float sum=0;
+  sum = f(a) + f(b);
+  for(int i=1; i<n; i++){
+    sum= sum+ 2*f((a+i*h));
+  }
+  sum = (sum*h)/2.0;
+  printf("Answer: %f", sum);
+}
